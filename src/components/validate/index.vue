7496293befd2589be889d552fd3852246df86abb
Coding rev <fernando.lobo.prez@gmail.com>
2025-08-11 01:00:05 -0300

    04-Vee validate
    04-Vee validate

--- src/components/validate/index.vue
+++ src/components/validate/index.vue
@@ -1,12 +1,42 @@
 <template>
+  <Form @submit="onSubmit">
     <div class="mb-3">
-        <label for="name">Name</label>
+      <label for="name">Name</label>
+      <Field 
+        name="name" 
+        :rules="[isRequired,validateName]"
+        placeholder="Enter your name"
+        class="form-control"
+      />
+      <ErrorMessage name="name" as="div" v-slot="{message}">
+        <div class="alert alert-danger" role="alert">
+          {{ message }}
+        </div>
+      </ErrorMessage>
     </div>
 
     <button class="btn btn-primary">
       Submit
     </button>
+  </Form>
 </template>
 
 <script setup>
+  import { Field, Form, ErrorMessage } from 'vee-validate';
+
+  const isRequired = (value) => {
+    if(!value) { return 'The field is required' }
+    return true
+  }
+  const validateName = (value) => {
+    if(value !== 'steve'){ return true }
+    return 'Your are not allowed steve !!'
+  }
+
+  function onSubmit(values,{ resetForm }){
+    console.log(values) /// api posts 
+    resetForm();
+  }
+
+
 </script>
